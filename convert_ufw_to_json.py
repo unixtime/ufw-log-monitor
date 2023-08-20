d1b090f63474689ee3bfb1018783bab8d19ce7f9
Hassan El-Masri <hassan@unixtime.com>
2023-08-20 14:43:11 -0700

    Improve logging

--- convert_ufw_to_json.py
+++ convert_ufw_to_json.py
@@ -1,28 +1,34 @@
 #!/usr/bin/env python3
+
 import os
 import re
 import json
 import maxminddb
 import ipaddress
 import psycopg2
-
 import subprocess
 from datetime import datetime
 import logging
+import time  # For adding sleep delay in subprocess management
 
 # Constants
+DEBUG_MODE = True  # Set to False to disable debugging output
+USE_DATABASE = False  # Set to False to only generate the JSON file
 LOG_FILE_PATH = "/var/log/ufw.log"
 GEOIP_DB_PATH = "/usr/share/GeoIP/GeoLite2-City.mmdb"
 OUTPUT_LOG_FILE = "ufw.log.json"
 BATCH_SIZE = 4
+RETRY_LIMIT = 3  # For subprocess management
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 LOG_PATTERN = re.compile(
-    r"SRC=(?P<ip_src>[^ ]+) DST=(?P<ip_dst>[^ ]+) "
-    r"PROTO=(?P<proto>[^ ]+)(?: SPT=(?P<spt>[^ ]+))?(?: DPT=(?P<dpt>[^ ]+))?")
+    r"(?P<date>[\w\s:]+) srv kernel: \[\s?\d+\.\d+] \[(?P<action>UFW \w+)] "
+    r".*?SRC=(?P<ip_src>[^ ]+) DST=(?P<ip_dst>[^ ]+) "
+    r".*?PROTO=(?P<proto>[^ ]+)(?:.*?SPT=(?P<spt>\d+))?(?:.*?DPT=(?P<dpt>\d+))?"
+)
 
 
 def get_year_based_on_time(log_time):
@@ -32,7 +38,7 @@ def get_year_based_on_time(log_time):
     return now.year if log_hour <= current_hour else now.year - 1
 
 
-def close_resource(resource, name):
+def safe_close(resource, name):
     """Safely closes a given resource."""
     try:
         if resource:
@@ -50,29 +56,48 @@ def load_maxmind_db():
 
 def connect_db():
     try:
-        return psycopg2.connect(
+        conn = psycopg2.connect(
             host=os.environ.get("DB_HOST", "your_host"),
             database=os.environ.get("DB_NAME", "your_database"),
             user=os.environ.get("DB_USER", "your_user"),
             password=os.environ.get("DB_PASSWORD", "your_password"),
         )
+        return conn, conn.cursor()
     except Exception as error:
         raise RuntimeError(f"Error connecting to database: {error}")
 
 
 def process_log_line(logs_line, logs_reader):
+    if "UFW BLOCK" in logs_line and "PROTO=TCP" in logs_line and ("SPT=" not in logs_line or "DPT=" not in logs_line):
+        print(f"Suspicious Log Line: {logs_line}")
+    if DEBUG_MODE:
+        print(f"Processing: {logs_line}")
     if "UFW" not in logs_line:
+        if DEBUG_MODE:
+            print(f"Skipped Line: {logs_line}")
         return None
 
     match = LOG_PATTERN.search(logs_line)
     if not match:
+        if DEBUG_MODE:
+            print(f"Unmatched Log Line: {logs_line}")
         return None
 
+    ufw_action = match.group('action')  # UFW BLOCK, UFW AUDIT, UFW ALLOW
     ip_src = match.group('ip_src')
     ip_dst = match.group('ip_dst')
     proto = match.group('proto')
     spt = match.group('spt')
     dpt = match.group('dpt')
+
+    if not spt or not dpt and proto in ["TCP", "UDP"]:
+        if DEBUG_MODE:
+            print(f"Missing SPT or DPT in Log Line: {logs_line}")
+        return None
+
+    # Debugging statement based on DEBUG_MODE
+    if DEBUG_MODE:
+        print(f"Parsed Log: Action: {ufw_action}, SRC: {ip_src}, DST: {ip_dst}")
 
     if ipaddress.ip_address(ip_src).is_private or ipaddress.ip_address(ip_dst).is_private:
         return None
@@ -81,8 +106,12 @@ def process_log_line(logs_line, logs_reader):
     timestamp = datetime.strptime(f"{year} {logs_line[0:15]}", "%Y %b %d %H:%M:%S")
     geo_src = get_geolocation(ip_src, logs_reader)
 
+    if geo_src is None:
+        geo_src = {}
+
     jlog_entry = {
         "timestamp": timestamp.strftime("%Y-%m-%d %H:%M:%S"),
+        "action": ufw_action,
         "ip_src": ip_src,
         "ip_dst": ip_dst,
         "proto": proto,
@@ -90,6 +119,10 @@ def process_log_line(logs_line, logs_reader):
         "dpt": dpt,
         "geo_src": geo_src,
     }
+
+    # Debugging statement based on DEBUG_MODE
+    if DEBUG_MODE:
+        print(f"Returning processed log entry: {jlog_entry}")
 
     return jlog_entry
 
@@ -110,15 +143,16 @@ def get_geolocation(ip, geo_reader):
 
 def insert_logs(db_log_entries, db_cursor):
     query = """
-        INSERT INTO ufw_logs (timestamp, ip_src, ip_dst, proto, spt, dpt, city_name, country_name,
+        INSERT INTO ufw_logs (timestamp, action, ip_src, ip_dst, proto, spt, dpt, city_name, country_name,
         latitude, longitude, postal_code, subdivision_name)
-        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
     """
     data = []
     for log in db_log_entries:
         geo_src = log.get("geo_src", {})
         data.append((
             log["timestamp"],
+            log["action"],
             log["ip_src"],
             log["ip_dst"],
             log["proto"],
@@ -136,53 +170,66 @@ def insert_logs(db_log_entries, db_cursor):
 
 # Main execution
 reader = load_maxmind_db()
-connection = connect_db()
-cursor = connection.cursor()
+
+if USE_DATABASE:
+    connection, cursor = connect_db()
+else:
+    connection, cursor = None, None
 
 logger.info(f"Service is running, monitoring {LOG_FILE_PATH}...")
 process = None
+retry_count = 0
+
 try:
-    process = subprocess.Popen(["tail", "--follow=name", LOG_FILE_PATH], stdout=subprocess.PIPE)
-    log_entries = []
+    process = subprocess.Popen(["tail", "--follow=name", LOG_FILE_PATH],
+                               stdout=subprocess.PIPE, universal_newlines=True)
 
-    for line in iter(process.stdout.readline, ""):
-        # Check if process is still running. If not, restart it.
-        poll_status = process.poll()
-        if poll_status is not None:
-            logger.warning("tail process was interrupted. Restarting...")
-            if poll_status is None:  # If process is still running
-                process.terminate()  # Terminate the old process
-                process.wait()  # Wait for the process to terminate
-            process = subprocess.Popen(["tail", "--follow=name", LOG_FILE_PATH], stdout=subprocess.PIPE)
-            continue  # Go to the next iteration to start reading from the new process
+    while process.poll() is None:  # Continue processing while the tail process is running
+        logs = []
 
-        log_entry = process_log_line(line.decode("utf-8"), reader)
-        if log_entry:
-            log_entries.append(log_entry)
-            with open(OUTPUT_LOG_FILE, "a") as outfile:
-                json.dump(log_entry, outfile)
-                outfile.write("\n")
-            logger.info(f"Processed log entry for {log_entry['ip_src']} -> {log_entry['ip_dst']}")
+        for _ in range(BATCH_SIZE):
+            logs.append(process.stdout.readline().strip())
 
-        if len(log_entries) >= BATCH_SIZE:
-            insert_logs(log_entries, cursor)
-            connection.commit()
-            log_entries = []
-            logger.info("Batch inserted into the database.")
+        # Processing logs
+        jlog_entries = [process_log_line(log, reader) for log in logs if log]
+        jlog_entries = [entry for entry in jlog_entries if entry]
 
-    if log_entries:  # Insert any remaining entries
-        insert_logs(log_entries, cursor)
-        connection.commit()
-        logger.info("Final batch inserted into the database.")
+        # Write to JSON File
+        with open(OUTPUT_LOG_FILE, "a") as f:
+            json.dump(jlog_entries, f, indent=2)
 
-except Exception as e:
-    logger.error(f"Error: {e}")
+        # If database is enabled, insert logs into the database
+        if USE_DATABASE and jlog_entries:
+            try:
+                insert_logs(jlog_entries, cursor)
+                connection.commit()
+            except Exception as e:
+                logger.error(f"Error inserting logs into the database: {e}")
+                connection.rollback()
 
+        time.sleep(0.5)  # Avoids excessive CPU usage, can be adjusted as necessary
+
+except KeyboardInterrupt:
+    logger.info("Gracefully stopping the service...")
+
+except Exception as ex:
+    retry_count += 1
+    logger.error(f"An error occurred: {ex}")
+
+    if retry_count <= RETRY_LIMIT:
+        logger.info(f"Retrying ({retry_count}/{RETRY_LIMIT})...")
+    else:
+        logger.error(f"Exceeded retry limit. Exiting.")
 
 finally:
-    close_resource(cursor, "cursor")
-    close_resource(connection, "database connection")
-    close_resource(reader, "MaxMind database reader")
-    if process and hasattr(process, 'terminate'):
+    if process:
         process.terminate()
-    logger.info("Service stopped.")
+
+    safe_close(reader, "GeoIP Reader")
+
+    if cursor:
+        cursor.close()
+    if connection:
+        connection.close()
+
+logger.info("Service has stopped.")
